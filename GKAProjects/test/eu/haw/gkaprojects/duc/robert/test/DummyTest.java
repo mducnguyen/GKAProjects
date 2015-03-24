eef30300206fd8e92abc558d18882f9bda9f5178
Robert <rscheffel00@gmail.com>
2015-03-24 22:59:32 +0100

    add Testfolder

---
+++ GKAProjects/test/eu/haw/gkaprojects/duc/robert/test/DummyTest.java
@@ -0,0 +1,16 @@
+package eu.haw.gkaprojects.duc.robert.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DummyTest 
+{
+	
+	@Test
+	public void testMethodOne()
+	{
+		assertTrue(true);
+	}
+}
